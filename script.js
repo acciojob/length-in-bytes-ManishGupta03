978b99cfe184fbe11b6fa659cba5538a280b777c
Manish Gupta <117648576+ManishGupta03@users.noreply.github.com>
2023-10-07 09:52:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const byteSize = (str) => {
   // write your code here
+	// Create a TextEncoder object
+    const encoder = new TextEncoder();
+
+    // Encode the string to bytes using UTF-8 encodinga 
+    return encoder.encode(str).length;
+
+    // Return the length of the encoded byte array
+    // return encoded.length;
+	
 };
 
 // Do not change the code below
